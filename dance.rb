61cf738103836cfe70ee235897c73896ca2edd30
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 14:52:22 +0000

    Done.

---
+++ dance.rb
@@ -0,0 +1,71 @@
+def first_steps?
+  loop do 
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Stop"
+    sleep(1)
+
+  end
+end
+  # Write a loop that outputs "Right foot back", "Left foot back",
+  # "Right foot back", and "Stop", with sleep after each
+
+def a_few_more_steps?
+  loop do 
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Stop"
+    sleep(1)
+    puts "Right foot steps right and back"
+    sleep (0.5)
+    puts "Left foot crosses over right"
+    sleep (0.5)
+    puts "Right foot steps right"
+
+  end
+end
+  # Write a loop that outputs the first two sets of steps in the Two-Step
+
+def how_many_steps?
+  steps = 0
+  loop do
+    steps += 1 
+    puts steps
+    if steps %2 == 0
+      puts "Left"
+    else
+      puts "Right"
+      sleep (0.5)
+    end
+  # Write a loop that fulfills all criteria from Part 3 of the README.md
+  end
+end 
+
+def break_dance
+  steps = 0
+  loop do 
+    steps += 1
+    puts steps
+    if steps % 2 == 0 
+      puts "Left"
+    else 
+      puts "Right"
+    end
+    sleep (0.5)
+    if steps == 6
+      break
+    end
+  end
+end
+
+puts break_dance
+  # Write a solution that uses the same code as how_many_steps?, but breaks the
+  # loop if steps is equal to 6
