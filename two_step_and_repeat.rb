047c40262e64e18fbf6ee44391813a6fa77f799f
Hope Zouzas <hmzouzas@gmail.com>
2018-07-11 14:07:15 +0000

    Done.

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,9 +1,32 @@
 def first_steps
-  # Write a loop that outputs "Right foot back", "Left foot back",
-  # "Right foot back", and "Stop", with sleep after each
+  loop do 
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
+end
 end
 
 def a_few_more_steps
+    loop do 
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
+  "Right foot steps right and back"
+  "Left foot crosses over right"
+  "Right foot steps right"
+end 
+end
+
   # Write a loop that outputs the first two sets of steps in the Two-Step
 end
 
---
+++ until.rb
@@ -0,0 +1,8 @@
+def hungry.gp
+  mango = 0 
+  until mango == 3 
+  puts "Give me more"
+  mango += 1 
+end
+end
+puts hungry.gp 
