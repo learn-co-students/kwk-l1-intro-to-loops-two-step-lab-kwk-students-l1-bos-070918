f3caf50101d053aff665afefe7b0e26cf4bc9267
Hope Zouzas <hmzouzas@gmail.com>
2018-07-11 14:17:09 +0000

    Done.

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -28,13 +28,43 @@ end
 end
 
   # Write a loop that outputs the first two sets of steps in the Two-Step
-end
 
-def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
+
+def how_many_steps
+  steps = 0 
+  loop do
+    steps += 1 
+     puts steps 
+    if steps % 2 == 0 
+      puts "left"
+    else 
+      puts "right"
+    end
+    sleep(0.5)
+  end
 end
+      
+  # Write a loop that fulfills all criteria from Part 3 of the README.md
 
 def break_dance
+  steps = 0 
+  loop do 
+    steps += 1 
+    puts steps
+    if steps % 2 == 0
+      puts "left"
+    else 
+      puts "right"
+    end 
+    sleep(0.5)
+    if steps == 6 
+      break
+    end
+  end
+end 
+
+break_dance 
+
   # Write a solution that uses the same code as how_many_steps?, but breaks the
   # loop if steps is equal to 6
-end
+
