1fa6f20f7060fc5da15108adf63d051e7dcc0f0e
Sidneyyy <catitude14@outlook.com>
2018-07-11 14:47:03 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,27 @@
+loop do 
+  puts "han"
+  puts "yolo"
+  break
+end
+
+puts "and his frien miter fly jedi"
+
+def hungrey_mn
+  kitkat = 100 
+  while kitkat > 95
+  puts "gimme kat"
+  kitkat -= 1
+end
+end
+
+hungrey_mn
+
+def hoongrey_mn
+  mango = 0
+  until mango == 3
+  puts "gimmi a man...then go"
+  mango += 1
+end
+end
+
+hoongrey_mn
